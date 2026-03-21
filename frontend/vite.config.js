c815f47e70d2fd35d9e9b99517e481880d41fe49
Shailesh Sulakay <shaileshsulakay@gmail.com>
2026-03-21 16:51:54 +0530

    port issue

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   preview: {
-    port: 4173,
+    port: process.env.PORT || 4173,
     host: true,
     allowedHosts: 'all'
   },
